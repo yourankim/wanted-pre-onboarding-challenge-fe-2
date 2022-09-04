3a0a9ae31643d9e8611e61fe463371cf9419a3d9
yourankim <youran.dev@gmail.com>
2022-09-04 22:37:42 +0900

    feat: todo type 정의

--- src/model/todo.ts
+++ src/model/todo.ts
@@ -1,6 +1,15 @@
 /**
  * @typedef {Object} Todo
+ * @property {string} id - id
+ * @property {string} content - 내용
+ * @property {boolean=false} isDone - 완료여부
+ * @property {string} category
+ * @property {string} [tags] - 태그들
  */
-interface Todo {
-  id: String;
-}
+const Todo = {
+  id: '',
+  content: '',
+  isDone: false,
+  category: '',
+  tags: '',
+};
