5d564f3a1f44b6c583fa7bca503c9c0b8c1b4b6f
yourankim <youran.dev@gmail.com>
2022-09-04 23:01:32 +0900

    feat: todoService 추가

--- src/model/todo.ts
+++ src/model/todo.ts
@@ -2,7 +2,7 @@
  * @typedef {Object} Todo
  * @property {string} id - id
  * @property {string} content - 내용
- * @property {boolean=false} isDone - 완료여부
+ * @property {boolean} isDone - 완료여부
  * @property {string} category
  * @property {string} [tags] - 태그들
  */
---
+++ src/service/todoService.ts
@@ -0,0 +1,45 @@
+/**
+ * 새로운 Todo를 등록한다.
+ * @param {Todo} todo
+ */
+const createTodo = (todo) => {};
+
+/**
+ * 등록된 모든 todo를 조회한다.
+ */
+const getAllTodos = () => {};
+
+/**
+ * id로 특정 todo만 조회한다.
+ */
+const getTodoById = (todoId) => {};
+
+/**
+ * Todo를 수정한다.
+ * @param {Todo} todo
+ */
+const updateTodo = (todo) => {};
+
+/**
+ * Todo를 삭제한다.
+ * @param {string} todoId
+ */
+const deleteTodo = (todoId) => {};
+
+/**
+ * 등록된 모든 Todo를 삭제한다.
+ */
+const deleteAllTodos = () => {};
+
+/**
+ * Todo에 등록된 특정 태그만 삭제한다.
+ * @param todoId 태그를 삭제할 todo의 id
+ * @param tag 삭제할 태그
+ */
+const deleteTodoTag = (todoId, tag) => {};
+
+/**
+ * Todo의 모든 태그를 삭제한다.
+ * @param todoId 태그를 삭제할 todo의 id
+ */
+const deleteAllTodoTag = (todoId) => {};
